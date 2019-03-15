2806e1e4a53a02fa29a915fcf3842e5536c8e863
Suki <46114711+suki-sahota@users.noreply.github.com>
2019-03-14 17:52:24 -0700

    Add files via upload

--- Craps.java
+++ Craps.java
@@ -4,7 +4,7 @@
  */
 import java.util.*;
 public class Craps {
-    //constant for number of games
+    // Constant for number of games
     public static final int GAMES = 10000;
     public static void main(String[] args) {
         play();
@@ -16,7 +16,7 @@ public class Craps {
         Random rand = new Random();
         int numberOfWin = 0, numberOfL = 0, thePoint = 0, comeOutRoll = 0;
         boolean playAgain = true;
-        //while loop to control if user plays again
+        // While loop to control if user plays again
         while (playAgain) {
             for (int i = 0; i < GAMES; i++) {
                 int dice1 = rand.nextInt(6) + 1;
@@ -32,7 +32,7 @@ public class Craps {
                 else if (x.equalsIgnoreCase("thePoint")) {
                     thePoint = comeOutRoll;
                 }
-                //if you don't win/lose on first roll, keep rolling
+                // If you don't win/lose on first roll, keep rolling
                 String y = keepRolling(thePoint);
                 if (y.equalsIgnoreCase("seven")) {
                     numberOfL++;
@@ -54,7 +54,7 @@ public class Craps {
         System.out.println("Have a nice day. GoodBye");
     }
     
-    //come out roll returns a string
+    // Come out roll returns a string
     public static String winOrLose(int comeOutRoll) {
         String x = "";
         if (comeOutRoll == 7 || comeOutRoll == 11) {
@@ -80,7 +80,7 @@ public class Craps {
         System.out.println("We will simulate 10,000 games\nLets start playing!\n");
     }
     
-    //keepRolling method returns a string
+    // keepRolling method returns a string
     public static String keepRolling(int thePoint) {
         Random rand = new Random();
         String y = "";
@@ -101,7 +101,7 @@ public class Craps {
         return y;
     }
     
-    //probability is in decimal form
+    // Probability is in decimal form
     public static double winProbability(double wins, double losses) {
         double probability = wins / (wins + losses);
         return probability;    
